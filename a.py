74d504dc1a3eb795bdd8552b289431fbe9a46f28
SNcoder <88935904+SNcoder@users.noreply.github.com>
2023-01-31 19:24:53 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+# age=12
+# print("my age is :",age)
+# print(f"my age is :{age}")
+# # we start index always 0
+# name="surya"
+# age=22
+# # print("my name is {} and age is {}".format(name , age))
+# #place holder
+# be="cs"
+# print("my name is {firstname} and age is {firstage}".format(firstname=name , firstage=age))
+# print(f"my name is {name} and my  age is {age} and my degree is {be} ")
+# age=12
+# if age<=18:
+#     print("you are eligible to vote")
+age=int(input("enter your age"))
+if age==18:
+    print("you are eligible to vote")
